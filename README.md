b0420099f72ee05f74f2400432a0dba4bf74f777
SpencerMax <54999424+SpencerMax@users.noreply.github.com>
2020-09-05 22:00:29 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,68 @@
+🌐 opensource-website
+=================
+
+
+
+Spencer Lander's personal website based on framework provied by Embark Studios
+
+## About
+
+This is a static site made using Vue.js.
+
+Project data is provided by a small JSON file, but in the future this should be grabbed from an API.
+
+All non-project data and text is hardcoded for now, but in the future this should be provided by a CMS.
+
+We should also add routing, multiple pages, and all sorts of other things 🙂
+
+Banner image: https://www.maxpixel.net/Flat-Web-Computer-Banner-Gray-Banner-Design-Idea-2833953
+
+## Possible future additions
+
+accordion: https://codepen.io/Fatal7x/pen/eZOYNK
+
+
+### Adding or Changing Projects
+
+The site's data comes from `data.json`. You can add a new project by adding its info to this file.
+
+```javascript
+{
+  // Make sure the name is exactly the same as the GitHub repo name
+  "name": "texture-synthesis",
+  // The most important part
+  "emoji": "🎨",
+  // Add tags that you find relevant, as a comma-separated array
+  "tags": ["rust"],
+  // Short description to display on the card
+  "description": "Example-based texture synthesis written in Rust",
+  
+  // The following fields are only required if the project will be featured:
+  "featured": true,
+  // Longer description, displayed on the featured card
+  "extendedDescription": "A light Rust API for Multiresolution Stochastic Texture Synthesis, a non-parametric example-based algorithm for image generation.",
+  // URL to an image to display on the featured card
+  "featureImage": "https://camo.githubusercontent.com/c279dea27db2c10f64cd27563d8d7cc86048c5c1/68747470733a2f2f692e696d6775722e636f6d2f43735a6f5350532e6a7067"
+}
+```
+
+### Adding or Changing Category Sections
+
+You can insert a section showing all projects with a specified tag by putting the following into `index.html`:
+
+```html
+<!-- Replace "rust" with your tag -->
+<project-category
+  tag="rust"
+  v-bind:projects="projectsWithTag('rust')"
+></project-category>
+```
+
+## License
+
+Licensed under either of
+
+* Apache License, Version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or http://www.apache.org/licenses/LICENSE-2.0)
+* MIT license ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
+
+at your option.
---
+++ script.js
@@ -0,0 +1,100 @@
+const { Vue } = window;
+
+const sharedMethods = {
+  methods: {
+    repoUrl(project) {
+      return `${project.url}`;
+    },
+    starButton(project) {
+      return `${project.name}`;
+    },
+  },
+};
+
+Vue.component('tags', {
+  props: ['tags'],
+  template: `
+    <div class="tags">
+      <div v-for="tag in tags" v-bind:class="'tag tag-' + tag">
+        <a v-bind:href="tagUrl(tag)">{{ tag }}</a>
+      </div>
+    </div>
+  `,
+  methods: {
+    tagUrl(tag) {
+      return `/tags?tag=${tag}`;
+    },
+  },
+});
+
+Vue.component('project-category', {
+  mixins: [sharedMethods],
+  props: ['projects', 'tag'],
+  template: `
+    <section class="category">
+      <h2 class="category-title">Our <span class="category-tag">{{ tag }}</span> projects</h2>
+      <div class="projects-container" v-bind:id="tag">
+        <a v-bind:href="repoUrl(p)" class="project" v-for="p in projects">
+          <div>
+            <h3 class="title">
+              <span class="emoji">{{ p.emoji }}</span>
+              {{ p.name }}
+            </h3>
+            <p v-html="p.description"></p>
+            <tags v-bind:tags="p.tags"></tags>
+          </div>
+          <iframe class="star-button" v-bind:src="starButton(p)" frameborder="0" scrolling="0" width="160px" height="30px"></iframe>
+        </a>
+      </div>
+    </section>
+  `,
+});
+
+fetch('./data.json').then((response) => response.json()).then((data) => {
+  new Vue({ // eslint-disable-line no-new
+    el: '#app',
+    mixins: [sharedMethods],
+    data: {
+      showSearch: false,
+      search: '',
+      ...data,
+    },
+
+    computed: {
+      featuredProjects() {
+        return this.projects.filter((p) => p.featured);
+      },
+      alphabetisedProjects() {
+        const { projects } = this;
+        return projects.sort((a, b) => a.name.localeCompare(b.name));
+      },
+      searchedProjects() {
+        return this.projects.filter(
+          (p) => JSON.stringify(p).toLowerCase().includes(this.search.toLowerCase()),
+        );
+      },
+    },
+
+    methods: {
+      // Return a filtered array of all projects with a tag
+      projectsWithTag(tag) {
+        return this.projects.filter((p) => p.tags.includes(tag));
+      },
+      getTagFromUrl() {
+        return new URL(document.location).searchParams.get('tag');
+      },
+      openSearch() {
+        document.body.classList.add('search-open');
+        this.showSearch = true;
+        this.$nextTick(() => this.$refs.search.focus());
+      },
+      closeSearch() {
+        document.body.classList.remove('search-open');
+        this.search = '';
+        this.showSearch = false;
+      },
+    },
+  });
+}).catch((err) => {
+  console.log(`Failed to get project data: ${err}`); // eslint-disable-line no-console
+});
